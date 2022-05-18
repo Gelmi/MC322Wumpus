845c642f94bcdcd9f4fdb06e623519355a49c833
Icaro Chiabai <ra247171@oliveira.lab.ic.unicamp.br>
2022-05-18 16:21:37 -0300

    inicializando caverna

--- src/pt/c40task/l05wumpus/Caverna.java
+++ src/pt/c40task/l05wumpus/Caverna.java
@@ -3,6 +3,15 @@
 public class Caverna {
 	private Sala salas[][] = new Sala[4][4];	
 	
+	public Caverna() {
+		//Inicializando salas
+		for(int i = 0; i < 4; i++) {
+			for(int j = 0; j < 4; j++) {
+				this.salas[i][j] = new Sala();
+			}
+		}
+	}
+	
 	public void conecta(Componente componente) {
 		componente.setCaverna(this);
 		salas[componente.getPosX()][componente.getPosY()].conecta(componente);
--- src/pt/c40task/l05wumpus/Montador.java
+++ src/pt/c40task/l05wumpus/Montador.java
@@ -4,19 +4,23 @@ public class Montador {
 
 	public void montarCaverna(String cave[][], Caverna caverna) {
 		//Percorre a matriz cave instanciando o componente adequado na posicao certa
+		System.out.println(cave.length);
 		for(int i = 0; i < cave.length; i++) {
 			switch (cave[i][2]) {
 			case "P":
-				caverna.conecta(new Heroi(Integer.parseInt(cave[i][0]), Integer.parseInt(cave[i][1])));
+				caverna.conecta(new Heroi(Integer.parseInt(cave[i][0]) - 1, Integer.parseInt(cave[i][1]) - 1));
 			break;
 			case "W":
-				caverna.conecta(new Wumpus(Integer.parseInt(cave[i][0]), Integer.parseInt(cave[i][1])));
+				caverna.conecta(new Wumpus(Integer.parseInt(cave[i][0]) - 1, Integer.parseInt(cave[i][1]) - 1));
 			break;
 			case "B":
-				caverna.conecta(new Buraco(Integer.parseInt(cave[i][0]), Integer.parseInt(cave[i][1])));
+				caverna.conecta(new Buraco(Integer.parseInt(cave[i][0]) - 1, Integer.parseInt(cave[i][1]) - 1));
 			break;
 			case "O":
-				caverna.conecta(new Ouro(Integer.parseInt(cave[i][0]), Integer.parseInt(cave[i][1])));
+				caverna.conecta(new Ouro(Integer.parseInt(cave[i][0]) - 1, Integer.parseInt(cave[i][1]) - 1));
+			break;
+			case "_":
+				caverna.conecta(new Componente(Integer.parseInt(cave[i][0]) - 1, Integer.parseInt(cave[i][1]) - 1));
 			break;
 			}
 		}
