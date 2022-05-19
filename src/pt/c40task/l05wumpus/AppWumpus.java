e8a79160def81773f7d954b93fb0a9c4eae52298
Gelmi <ggelmisalvo@gmail.com>
2022-05-19 13:55:12 -0300

    lab final

--- src/pt/c40task/l05wumpus/AppWumpus.java
+++ src/pt/c40task/l05wumpus/AppWumpus.java
@@ -20,47 +20,55 @@ public class AppWumpus {
       
       if(arquivoMovimentos == null) {
 	      Controle controle = new Controle();
-	      montador.montarCaverna(cave, caverna, controle);
-	      System.out.println("=== Caverna");
-	      for (int l = 0; l < caverna.toStringMatrix().length ; l++) {
-	         for (int c = 0; c < caverna.toStringMatrix()[l].length; c++)
-	            System.out.print(caverna.toStringMatrix()[l][c] + ((c < caverna.toStringMatrix()[l].length-1) ? ", " : ""));
-	         System.out.println();
+	      Boolean cavernaValida = montador.montarCaverna(cave, caverna, controle);
+	      if(cavernaValida) {
+		      System.out.println("=== Caverna");
+		      for (int l = 0; l < caverna.toStringMatrix().length ; l++) {
+		         for (int c = 0; c < caverna.toStringMatrix()[l].length; c++)
+		            System.out.print(caverna.toStringMatrix()[l][c] + ((c < caverna.toStringMatrix()[l].length-1) ? ", " : ""));
+		         System.out.println();
+		      }
+		      System.out.println("Player: "+controle.getJogador());
+		      System.out.println("Score: "+controle.getHeroi().getPontuacao());
+	
+		      char status = 'P';
+		      while(jogoAtivo) {
+		    	  jogoAtivo = controle.pegarComandos();
+		          for (int l = 0; l < caverna.toStringMatrix().length ; l++) {
+		              for (int c = 0; c < caverna.toStringMatrix()[l].length; c++)
+		                 System.out.print(caverna.toStringMatrix()[l][c] + ((c < caverna.toStringMatrix()[l].length-1) ? ", " : ""));
+		              System.out.println();
+		           }
+		          System.out.println("Player: "+controle.getJogador());
+		          System.out.println("Score: "+controle.getHeroi().getPontuacao());
+		          if(controle.getHeroi().getCausa().equals(new String("Voce ganhou =D !!!"))) {
+		        	  status = 'W';
+		          } else if (controle.getHeroi().getCausa().equals(new String("Voce perdeu =( ..."))){
+		        	  status = 'P';
+		          }
+		          tk.writeBoard(caverna.toCharMatrix(), controle.getHeroi().getPontuacao(), status);
+		      }
+		      System.out.println(controle.getHeroi().getCausa());
+	      } else {
+	    	  System.out.println("Caverna Inválida");
 	      }
-	      System.out.println("Player: "+controle.getJogador());
-	      System.out.println("Score: "+controle.getHeroi().getPontuacao());
-
-	      char status = 'P';
-	      while(jogoAtivo) {
-	    	  jogoAtivo = controle.pegarComandos();
-	          for (int l = 0; l < caverna.toStringMatrix().length ; l++) {
-	              for (int c = 0; c < caverna.toStringMatrix()[l].length; c++)
-	                 System.out.print(caverna.toStringMatrix()[l][c] + ((c < caverna.toStringMatrix()[l].length-1) ? ", " : ""));
-	              System.out.println();
-	           }
-	          System.out.println("Player: "+controle.getJogador());
-	          System.out.println("Score: "+controle.getHeroi().getPontuacao());
-	          if(controle.getHeroi().getCausa().equals(new String("Voce ganhou =D !!!"))) {
-	        	  status = 'W';
-	          } else if (controle.getHeroi().getCausa().equals(new String("Voce perdeu =( ..."))){
-	        	  status = 'P';
-	          }
-	          tk.writeBoard(caverna.toCharMatrix(), controle.getHeroi().getPontuacao(), status);
-	      }
-	      System.out.println(controle.getHeroi().getCausa());
       } else {
           String movements = tk.retrieveMovements();
     	  Controle controle = new Controle("Automático");
-	      montador.montarCaverna(cave, caverna, controle);
-	      char status = 'P';
-	      for(int i = 0; i < movements.length(); i++) {
-	    	  jogoAtivo = controle.pegarComandos(Character.toString(movements.charAt(i)));
-	          if(controle.getHeroi().getCausa().equals(new String("Voce ganhou =D !!!"))) {
-	        	  status = 'W';
-	          } else if (controle.getHeroi().getCausa().equals(new String("Voce perdeu =( ..."))){
-	        	  status = 'P';
-	          }
-	          tk.writeBoard(caverna.toCharMatrix(), controle.getHeroi().getPontuacao(), status);
+	      Boolean cavernaValida = montador.montarCaverna(cave, caverna, controle);
+	      if(cavernaValida) {
+		      char status = 'P';
+		      for(int i = 0; i < movements.length(); i++) {
+		    	  jogoAtivo = controle.pegarComandos(Character.toString(movements.charAt(i)));
+		          if(controle.getHeroi().getCausa().equals(new String("Voce ganhou =D !!!"))) {
+		        	  status = 'W';
+		          } else if (controle.getHeroi().getCausa().equals(new String("Voce perdeu =( ..."))){
+		        	  status = 'P';
+		          }
+		          tk.writeBoard(caverna.toCharMatrix(), controle.getHeroi().getPontuacao(), status);
+		      }
+	      } else {
+	    	  System.out.println("Caverna Inválida");
 	      }
       }
 
--- src/pt/c40task/l05wumpus/Montador.java
+++ src/pt/c40task/l05wumpus/Montador.java
@@ -10,27 +10,32 @@ public class Montador {
 		}
 	}
 	
-	public void montarCaverna(String cave[][], Caverna caverna, Controle controle) {
+	public Boolean montarCaverna(String cave[][], Caverna caverna, Controle controle) {
 		//Percorre a matriz cave instanciando o componente adequado na posicao certa
 		for(int i = 0; i < cave.length; i++) {
 			switch (cave[i][2]) {
 			case "P":
 				caverna.conecta(new Heroi(Integer.parseInt(cave[i][0])-1, Integer.parseInt(cave[i][1])-1, controle));
-				nCadaComponente[0]+=1;
+				this.nCadaComponente[0]+=1;
 			break;
 			case "W":
 				caverna.conecta(new Wumpus(Integer.parseInt(cave[i][0])-1, Integer.parseInt(cave[i][1])-1));
-				nCadaComponente[1]+=1;
+				this.nCadaComponente[1]+=1;
 			break;
 			case "B":
 				caverna.conecta(new Buraco(Integer.parseInt(cave[i][0])-1, Integer.parseInt(cave[i][1])-1));
-				nCadaComponente[2]+=1;
+				this.nCadaComponente[2]+=1;
 			break;
 			case "O":
 				caverna.conecta(new Ouro(Integer.parseInt(cave[i][0])-1, Integer.parseInt(cave[i][1])-1));
-				nCadaComponente[3]+=1;
+				this.nCadaComponente[3]+=1;
 			break;
 			}
+		}
+		if(this.nCadaComponente[0] == 1 && this.nCadaComponente[1] == 1 && this.nCadaComponente[2] >=2 && this.nCadaComponente[2] <=3 && this.nCadaComponente[3] == 1) {
+			return true;
+		} else {
+			return false;
 		}
 	}
 }
